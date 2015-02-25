4eb84fded9f9455916f1b98b9db5aff014f8c840
patsykakaz <philippe@lesidecar.fr>
2015-02-25 18:45:22 +0100

    after patsy's approuval

--- js/main.js
+++ js/main.js
@@ -2,27 +2,17 @@ $(document).ready(function(){
     BlackBoxMarker = 0;
     i = 0;
     $('.box').each(function(){
-        refHeight = $(this).height();
-        captionHeight = $(this).children('.item-caption').outerHeight();
-        $(this).find('.layer-img').css('height', refHeight-captionHeight);
-        imgTarget = $(this).children('.layer-img').find('img');
-        if(imgTarget.height()>refHeight){
-            imgTarget.css('bottom', (imgTarget.height()-refHeight)/2);
-        }else{
-        	// imgTarget.css('height', imgTarget.height()-captionHeight+'px');
-        }
-        // $(this).children('.item-caption').css('height', captionHeight);
 
-        if(BlackBoxMarker < ($('.box').length)/4){
-            randNum = Math.floor((Math.random()*3)+1);
-            if(randNum == 3 && !($('.box:eq('+i+')').hasClass('bigbox'))){
-                // $('.box:eq('+i+')').addClass('blackbox');
-                BlackBoxMarker+=1; 
-            }
+        layer = $(this).children('.layer-img');
+        illustration = layer.children('.illustration');
+
+        if(illustration.width()<layer.width()){
+            illustration.css('height', 'auto').css('width', '100%').css('bottom',(illustration.height()-layer.height())/2);
         }
-        i +=1 ;
-    });
-    $('.box').each(function(){
+
+
+
+
         $(this).addClass('thumbnail');
     });
 });
