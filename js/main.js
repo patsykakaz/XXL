304169194cbccfa6cb4959c4c7d78d493f1f6b94
patsykakaz <patsykakaz@iMac-de-patsykakaz.local>
2015-03-11 15:11:01 +0100

    Gestion habillage finie

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 
 // Resizing box images
+
+$(document).ready(function(){
+    // $('.box').each(function(){
+        // $(this).addClass('thumbnail');
+    // });
+});
+
 $(window).load(function(){
     boxImageSizing();
+    placement_habillage();
+    place_navbarForm();
 });
 $(window).resize(function(){
     boxImageSizing();
+    placement_habillage();
 });
 // ./Resizing box images
 function boxImageSizing(){
@@ -18,7 +28,7 @@ function boxImageSizing(){
 }
 
 $(document).ready(function(){
-    // ? 
+    // Calle la largeur de #masterNav sur celle de #main
     $('#masterNav .container-fluid').width($('#main').width());
     // aligne les .navbar-nav de masterNav en bas de la navbar
     $('#masterNav .navbar-nav').each(function(){
@@ -48,7 +58,7 @@ $(document).ready(function(){
     // positionnement du logo pour la deployNav
     $('#logo_deploy').css('left', ($(window).width()-$('#main').outerWidth())/2-$('#logo_deploy').outerWidth());
     // Padding automatique selon la taille du logo
-    $('#logo_deploy').css('padding-top', ($('#contentNav').outerHeight()-$('#logo_deploy').outerHeight())/2);
+    $('#logo_deploy').css('padding-top', ($('#contentNav').outerHeight()-$('#logo_deploy').outerHeight())/2-10);
 })
 $(document).scroll(function(){
     nav = $('#contentNav');
@@ -56,13 +66,15 @@ $(document).scroll(function(){
     // Point de déploiment de la navBar secondaire
     trigger = $('#contentNav_layer').offset().top;
     if($(document).scrollTop() >= trigger){
+        place_navbarForm();
         nav.addClass('deployNav');
         setTimeout(function(){
             nav.css('top', '0');
             logo.addClass('on');
         },200);
         nav.children('.container-fluid').css('width', $('#main').outerWidth());
-    }else{
+    }else if($(document).scrollTop() <= trigger+50){
+        place_navbarForm();
             logo.removeClass('on');
             nav.removeClass('deployNav');
             nav.css('top', '');
@@ -70,3 +82,28 @@ $(document).scroll(function(){
     }
 });
 // ./Deploiment contentNav
+
+
+// Placement HABILLAGE
+function placement_habillage(){
+    target = $('#pubLayer');
+    ratioBG = 970/1870;
+    // if($(window).width()<970){
+        mainWidth = $('#main').outerWidth();
+        windowWidth = $(window).width();
+        target.css('background-size', (mainWidth/windowWidth)/ratioBG*100+'%');
+        $('#pubLayer').css('padding-top', 150+'px');
+    // }
+    if($(window).width()<970){
+        $('#pubLayer').css('padding-top', ($('#main').outerWidth()/970)*150+'px');
+    }
+}
+// ./Placement HABILLAGE
+
+
+function place_navbarForm(){
+    target = $('#contentNav .navbar-form');
+    target.css('margin-top', ($('#contentNav .container-fluid').height()-target.outerHeight())/2);
+}
+
+
